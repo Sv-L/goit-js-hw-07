ef712d9688322742c4eef56e79e5200de352a567
liulka_s <asl.warmhaus@gmail.com>
2023-04-29 23:48:58 +0200

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector('.gallery');
+
+const createGalleryItems = galleryItems.map(galleryItem => `
+    <li class='gallery__item'>
+        <a class='gallery__link'
+            href='${galleryItem.original}' >
+                <img class='gallery__image'
+                    src='${galleryItem.preview}' 
+                    alt='${galleryItem.description}'
+                 />
+        </a>
+    </li>`                  
+    ).join('');
+
+galleryListEl.insertAdjacentHTML('afterbegin', createGalleryItems);
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
